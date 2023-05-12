5cc8bdeab457178879c31bf76d78c77164acaacc
alfawakhryy <alfawakhryy@gmail.com>
2023-05-12 10:35:12 +0200

    Deployment Phase

---
+++ src/PomodoroTimer.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+const PomodoroTimer = () => {
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isRunning]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const pauseTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setTimeLeft(25 * 60);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  return (
+    <div>
+      <h1>Pomodoro Timer</h1>
+      <p>{formatTime(timeLeft)}</p>
+      <button onClick={isRunning ? pauseTimer : startTimer}>
+        {isRunning ? "Pause" : "Start"}
+      </button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
+};
+
+export default PomodoroTimer;
